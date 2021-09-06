67a99c80e9f6732ac8eee5fdcaa35379cc0f4d0d
Junaid Khan <junaid.khan.17@mountblue.tech>
2021-09-06 15:37:58 +0530

    initial commit: Task List

--- src/App.js
+++ src/App.js
@@ -1,24 +1,103 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import List from './List';
+import Alert from './Alert';
+
+const getLocalStorage = () => {
+  let list = localStorage.getItem('list');
+  if (list) {
+    return JSON.parse(list);
+  } else {
+    return [];
+  }
+};
 
 function App() {
+  const [name, setName] = useState('');
+  const [list, setList] = useState(getLocalStorage());
+  const [alert, setAlert] = useState({ show: false, type: '', msg: '' });
+  const [isEditing, setIsEditing] = useState(false);
+  const [editID, setEditID] = useState(null);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!name) {
+      showAlert(true, 'danger', 'please enter value');
+    } else if (name && isEditing) {
+      setList(
+        list.map((item) => {
+          if (item.id === editID) {
+            return { ...item, title: name };
+          }
+          return item;
+        })
+      );
+      setName('');
+      setEditID(null);
+      setIsEditing(false);
+      showAlert(true, 'success', 'value changed');
+    } else {
+      showAlert(true, 'success', 'item added to the list');
+      const newItem = { id: new Date().getTime().toString(), title: name };
+
+      setList([...list, newItem]);
+      setName('');
+    }
+  };
+
+  const showAlert = (show = false, type = '', msg = '') => {
+    setAlert({ show, type, msg });
+  };
+
+  const clearList = () => {
+    showAlert(true, 'danger', 'empty list');
+    setList([]);
+  };
+
+  const removeItem = (id) => {
+    showAlert(true, 'danger', 'item removed');
+    setList(list.filter((item) => item.id !== id));
+  };
+
+  const editItem = (id) => {
+    const curItem = list.find((item) => item.id === id);
+    setIsEditing(true);
+    setEditID(id);
+    setName(curItem.title);
+  };
+
+  useEffect(() => {
+    localStorage.setItem('list', JSON.stringify(list));
+  }, [list]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <section className='section-center'>
+      <form className='task-form' onSubmit={handleSubmit}>
+        {alert.show && <Alert {...alert} removeAlert={showAlert} list={list} />}
+
+        <h3>Task List</h3>
+        <div className='form-control'>
+          <input
+            type='text'
+            className='task-input'
+            placeholder='e.g. task 1'
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+          <button type='submit' className='btn submit-btn'>
+            {isEditing ? 'edit' : 'add'}
+          </button>
+        </div>
+      </form>
+
+      {list.length > 0 && (
+        <div className='task-container'>
+          <List items={list} removeItem={removeItem} editItem={editItem} />
+          <button className='clear-btn' onClick={clearList}>
+            clear items
+          </button>
+        </div>
+      )}
+    </section>
   );
 }
 
